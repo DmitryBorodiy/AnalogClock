0c00b9a6a5450529a1f3f19891b13731b34b1508
Dmitriy <57910623+DmitryBorodiy@users.noreply.github.com>
2022-08-19 23:53:42 +0300

    debug

---
+++ helpers/ThemeHelper.js
@@ -0,0 +1,26 @@
+﻿import {
+  StandardLuminance,
+  baseLayerLuminance,
+  fillColor,
+  allComponents,
+  provideFluentDesignSystem
+} from "https://unpkg.com/@fluentui/web-components";
+
+class ThemeHelper {
+  constructor() { }
+
+  GetCurrentTheme(){
+
+  }
+}
+
+const KnownDeviceTheme = {
+  Light: 0,
+  Dark: 1
+};
+
+provideFluentDesignSystem().register(allComponents);
+
+const domContainer = document.querySelector('#root');
+
+baseLayerLuminance.setValueFor(domContainer, StandardLuminance.DarkMode);
